07c37184651db3e51d87b60042c346b26c457b49
Walder Daniel <walder2@gmx.at>
2020-11-29 10:36:19 +0100

    update

--- src/services/issService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-
-const getLocation = async () => {
-  const response = await axios.get(`http://api.open-notify.org/iss-now.json`)
-  return response.data
-}
-
-export default { getLocation }
