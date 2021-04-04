6808511946369d16c18d6bb118728b3448862f21
Daniel <walder.daniel@gmx.at>
2021-04-04 15:56:12 +0200

    update

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -6,17 +6,17 @@ const scanArea = require('./utils/scanArea')
 require('dotenv').config()
 const botCommunication  = require('./botCommunication')
 
-const locationScheduler = schedule.scheduleJob('*/1 * * * *', async () => {
-    const response = await axios.get(`http://api.open-notify.org/iss-now.json`)
-
-    const locationLine = await database.selectAllRows('rowid, latitude, longitude', 'location')
-    if(locationLine.length === 0){
-        await database.insertRow('location', '(null, ?, ?)', [response.data.iss_position.latitude, response.data.iss_position.longitude])
-    }else{
-        await database.updateById('location', 'latitude = ?, longitude = ?', [response.data.iss_position.latitude, response.data.iss_position.longitude, 1])
-    }
-    scanArea.isISSAround()
-})
+//const locationScheduler = schedule.scheduleJob('*/1 * * * *', async () => {
+//    const response = await axios.get(`http://api.open-notify.org/iss-now.json`)
+//
+//    const locationLine = await database.selectAllRows('rowid, latitude, longitude', 'location')
+//    if(locationLine.length === 0){
+//        await database.insertRow('location', '(null, ?, ?)', [response.data.iss_position.latitude, response.data.iss_position.longitude])
+//    }else{
+//        await database.updateById('location', 'latitude = ?, longitude = ?', [response.data.iss_position.latitude, response.data.iss_position.longitude, 1])
+//    }
+//    scanArea.isISSAround()
+//})
 
 
 const astronomyScheduler = schedule.scheduleJob('40 10 * * *', async () => {
